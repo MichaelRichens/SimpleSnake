44488046f4cf34ad33891f8a2b2323324a6a8f8b
Michael Richens <michael.richens@gmail.com>
2023-07-17 14:16:20 +0100

    Added comments.

--- TextStrings.cs
+++ TextStrings.cs
@@ -11,8 +11,17 @@ namespace SimpleSnake
 	/// </summary>
 	static class TextStrings
 	{
+		/// <summary>
+		/// Provides some heading text to be displayed above the game when playing in the Console.
+		/// </summary>
+		/// <param name="pauseKey">The currently set pause key.</param>
+		/// <param name="quitKey">The currently set quit game key.</param>
+		/// <returns>The string to display.</returns>
 		internal static string ConsoleBoardHeading(ConsoleKey pauseKey, ConsoleKey quitKey) => $"Press {pauseKey} to pause, press {quitKey} to quit.";
 
+		/// <summary>
+		/// A lookup to provide the text to display for each memeber of the MainMenuOption enum.
+		/// </summary>
 		internal static Dictionary<MainMenuOption, string> MainMenu = new() {
 			{ MainMenuOption.Play, "Start Game" },
 			{ MainMenuOption.Quit, "Exit Game" },
